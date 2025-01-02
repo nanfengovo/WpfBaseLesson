046d2354c6fdb712021beb33782ab827dac8daca
nanfengovo <nanfengovo@gmail.com>
2025-01-03 00:20:50 +0800

    点击事件+绑定实现登录
    >问题：登录失败的情况不能清空页面文本框中的值

--- OAManage/OAManage/MainWindow.xaml.cs
+++ OAManage/OAManage/MainWindow.xaml.cs
@@ -19,9 +19,16 @@ namespace OAManage
         public MainWindow()
         {
             InitializeComponent();
+            //设置数据上下文
+            this.DataContext = this;
         }
 
-        
+        //账号
+        public string Account { get; set; }
+
+        //密码
+        public string Pwd { get; set; }
+
         /// <summary>
         /// 登录
         /// </summary>
@@ -29,19 +36,19 @@ namespace OAManage
         /// <param name="e"></param>
         private void Btn_Login(object sender, RoutedEventArgs e)
         {
-            string account = this.txtAccount.Text;
-            string pwd = this.txtPwd.Text;
+          
 
-            if(account == "longma" && pwd=="123" )
+            if(Account == "longma" && Pwd=="123" )
             {
                 MessageBox.Show("登录成功");
             }
             else
             {
-                MessageBox.Show("登录失败");
                 //清空文本框
-                this.txtAccount.Text = "";
-                this.txtPwd.Text = "";
+                this.Account = "";
+                this.Pwd = "";
+                MessageBox.Show("登录失败");
+                
             }
 
         }
